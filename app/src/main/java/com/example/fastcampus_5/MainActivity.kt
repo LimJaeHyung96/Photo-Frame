366a237f8d2b5664f7868ff908e9a2ac1b3d9f6f
LimJaeHyung96 <jhlim96@naver.com>
2022-03-04 17:51:08 +0900

    220304 finished

--- app/src/main/java/com/example/fastcampus_5/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus_5/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.fastcampus_5
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,15 +14,15 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private val photoAddButton : Button by lazy {
+    private val photoAddButton: Button by lazy {
         findViewById(R.id.photoAddButton)
     }
 
-    private val photoFrameStartButton : Button by lazy {
+    private val photoFrameStartButton: Button by lazy {
         findViewById(R.id.photoFrameStartButton)
     }
 
-    private val imageViewList : List<ImageView> by lazy {
+    private val imageViewList: List<ImageView> by lazy {
         mutableListOf<ImageView>().apply {
             add(findViewById(R.id.imageView11))
             add(findViewById(R.id.imageView12))
@@ -31,6 +33,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val imageUriList: MutableList<Uri> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,9 +43,9 @@ class MainActivity : AppCompatActivity() {
         initPhotoFrameStartButton()
     }
 
-    private fun initPhotoAddButton(){
+    private fun initPhotoAddButton() {
         photoAddButton.setOnClickListener {
-            when{
+            when {
                 ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
@@ -53,9 +57,24 @@ class MainActivity : AppCompatActivity() {
                     showPermissionPopUP()
                 }
                 else -> {
-                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
                 }
             }
+        }
+    }
+
+    private fun initPhotoFrameStartButton() {
+        photoFrameStartButton.setOnClickListener {
+            val intent = Intent(this, PhotoFrameActivity::class.java)
+            imageUriList.forEachIndexed { index, uri ->
+                intent.putExtra("photo$index", uri.toString())
+            }
+
+            intent.putExtra("photoListSize", imageUriList.size)
+            startActivity(intent)
         }
     }
 
@@ -66,13 +85,13 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when(requestCode){
+        when (requestCode) {
             1000 -> {
-                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //todo 권한이 부여가 된 것
                     getPhotos()
                 } else {
-                    Toast.makeText(this, "권한이 거부되어 있습니다.",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "권한이 거부되어 있습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
             else -> {
@@ -82,9 +101,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getPhotos() {
-        val intent= Intent(Intent.ACTION_GET_CONTENT)
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         startActivityForResult(intent, 2000) //선택된 컨텐츠가 넘어와야 하기 때문
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when (requestCode) {
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+                //if문에서 null 체크를 했기 때문에 안에서는 null타입 안 써도 됨
+                if (selectedImageUri != null) {
+
+                    if(imageUriList.size == 6){
+                        Toast.makeText(this, "사진은 최대 6장까지만 추가 가능합니다", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri) //위에서 add를 해줘서 size가 0이 될 일은 없음
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun showPermissionPopUP() {
@@ -94,13 +143,8 @@ class MainActivity : AppCompatActivity() {
             .setPositiveButton("권한추가") { _, _ ->
                 requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
             }
-            .setNegativeButton("취소",null)
+            .setNegativeButton("취소", null)
             .create()
             .show()
     }
-
-    private fun initPhotoFrameStartButton(){
-
-    }
-
 }
